5cbf55c1ba2e36a66a776299aa1cc5786aacff5f
Robbie Lannon <robbielannon@gmail.com>
2025-07-10 15:20:36 -0500

    basic route logic and additional pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, render_template, request
 
 app = Flask(__name__)
 
@@ -10,3 +10,31 @@ Session(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    posts = BlogPost.query.all()
+    return render_template('blog.html', posts=posts)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        results = BlogPost.query.filter(BlogPost.content.contains(query)).all()
+        return render_template('search.html', results=results, query=query)
+    else:
+        return render_template('search.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # TODO: save the message to a database or send an email
+        return render_template('contact.html', success=True)
+    return render_template('contact.html')
