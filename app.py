f7aff14e678b48b08ceeec60003ec9e0a59e269b
Robbie Lannon <robbielannon@gmail.com>
2025-07-10 14:21:38 -0500

    install Flask and initialize app.py file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
