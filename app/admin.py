d2419018b6ca1760bb1da970ed8c183b10b25aef
Robbie Lannon <robbielannon@gmail.com>
2025-07-23 12:34:41 -0500

    create and secure admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,21 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from app.models import db, BlogPost, Author
+from flask import redirect, url_for
 
-admin = Admin(name='Admin Panel', template_mode='bootstrap4')
+class SecureAdminIndexView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not current_user.is_authenticated or not current_user.is_admin:
+            return redirect(url_for('auth.login'))
+        return super().index()
 
 class SecureModelView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
+
+admin = Admin(name='Admin Panel', template_mode='bootstrap4', index_view=SecureAdminIndexView())
 
 admin.add_view(SecureModelView(BlogPost, db.session))
 admin.add_view(SecureModelView(Author, db.session))
--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_session import Session
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 mail = Mail()
 session_manager = Session()
+login_manager = LoginManager()
